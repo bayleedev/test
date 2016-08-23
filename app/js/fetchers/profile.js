ab2129d63c7dc010ac88b77f2f62a9aabfee6e4f
Blaine Schmeisser <blainesch@gmail.com>
2016-08-23 09:18:28 -0700

    Create a fetchall.

--- app/js/fetchers/profile.js
+++ app/js/fetchers/profile.js
@@ -5,14 +5,50 @@ function info (name) {
     })
 }
 
-function repos (name) {
-  return fetch(`https://api.github.com/users/${name}/repos`)
+function fetchAll (url, isDone, page) {
+  const currentPage = page || 1
+  const fetchUrl = url + '?page=' + (currentPage)
+  return fetch(fetchUrl).then((res) => {
+    return isDone(res.clone()).then((done) => {
+      if (done) {
+        return [res]
+      } else {
+        return fetchAll(url, isDone, currentPage + 1).then((resArray) => {
+          resArray.unshift(res)
+          return resArray
+        })
+      }
+    })
+  })
+}
+
+function events (name) {
+  return fetch(`https://api.github.com/users/${name}/events`)
     .then((res) => {
       return res.json()
     })
 }
 
+function repos (name) {
+  return fetchAll(`https://api.github.com/users/${name}/repos`, (res) => {
+    return res.json().then((repos) => {
+      return repos.length !== 30
+    })
+  }).then((responses) => {
+    return Promise.all(responses.map((response) => {
+      return response.json()
+    }))
+  }).then((bodies) => {
+    return bodies.reduce((memo, body) => {
+      return memo.concat(body)
+    }, [])
+  })
+}
+
 export function profileFetcher (name) {
+  const eventPromises = repos(name).then((respos) => {
+    debugger
+  })
   return Promise.resolve({
     login: 'blainesch',
     repos: [
